ae6fcc227b05e24d481f6f67387c1c85e04d2f02
David Domínguez <dedc_011596@hotmail.com>
2023-04-20 13:32:44 -0600

    Se agrega otro tipo

--- apps/bonos/models.py
+++ apps/bonos/models.py
@@ -5,6 +5,8 @@ from django.contrib.postgres.fields import JSONField
 from utils.models import ControlInfo
 
 class Bono(ControlInfo):
+    class Meta:
+        ordering = ('abonado__name',)
     identifier = 'OMB'
     tipos = (
         ('abonado', 'Abonado'),
@@ -17,8 +19,9 @@ class Bono(ControlInfo):
         ('network', 'Network'),
         ('directiva', 'Directiva'),
         ('admin', 'Administración'),
-        ('tarjeton', 'Tarjetón Est')
+        ('tarjeton', 'Tarjetón Est'),
+        ('zona_comercial', 'Zona Comercial')
     )
-    tipo = models.CharField(choices=tipos, max_length=10, default='cortesia')
+    tipo = models.CharField(choices=tipos, max_length=100, default='cortesia')
     abonado = JSONField(editable=False) #(nombre, teléfono, correo opcional)
     ubicacion = JSONField(editable=False) #(sección, fila, no. butaca)
